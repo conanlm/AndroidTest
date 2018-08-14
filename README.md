1f3525193dccf096ae779ca7b9f9a6a246f1e0b5
conanlm <conanlm3@gmail.com>
2018-08-14 15:33:18 +0800

    2018/08/14 基地

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 
 * https://www.bilibili.com/video/av18008899/ 现在在看B站视频
 
-2.1.1 按视频改LinearLayout，效果没刷新，百度后Failed to load AppCompat ActionBar with unknown error.
+- 2.1.1 按视频改LinearLayout，效果没刷新，百度后Failed to load AppCompat ActionBar with unknown error.
 修改styles.xml文件parent属性前面加上Base后，效果刷新
+
+- 2.2 从API level 24开始，fromHtml(String)被废弃，使用fromHtml(String source, int flags) 代替
+flags:
+
+    FROM_HTML_MODE_COMPACT：html块元素之间使用一个换行符分隔
+    FROM_HTML_MODE_LEGACY：html块元素之间使用两个换行符分隔
+
+
--- app/src/main/java/com/conanlm/helloworld/MainActivity.java
+++ app/src/main/java/com/conanlm/helloworld/MainActivity.java
@@ -1,13 +1,40 @@
 package com.conanlm.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button mbtnTextView;
+    private Button mBtnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mbtnTextView =findViewById(R.id.btn_textview);
+        mbtnTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent(MainActivity.this,TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mBtnButton =findViewById(R.id.btn_button);
+        mBtnButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent(MainActivity.this,ButtonActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
